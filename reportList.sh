4c7c3ac6eb8a51bff147c4b33f024fc188297b5a
Benoît Sauvère <benoit.sauvere@teevity.com>
2015-01-14 08:51:45 +0100

    Added a script to report a list of twitter accounts

---
+++ reportList.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TWITTER_USERNAME=$1
+TWITTER_PASSWORD=$2
+
+while read LINE
+do
+  
+    echo "Reporting ${LINE}";
+
+    phantomjs --ssl-protocol tlsv1 twitter-report-user-account.js "${TWITTER_USERNAME}" "${TWITTER_PASSWORD}" "${LINE}"
+  
+done < listeComptes.txt
